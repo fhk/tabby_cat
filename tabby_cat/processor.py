d43885391c3399b1796b291e3e2db610cfff454f
plantz <f.kauker@gmail.com>
2020-07-03 15:14:19 -0700

    convert node ids only

--- tabby_cat/processor.py
+++ tabby_cat/processor.py
@@ -202,7 +202,7 @@ class Processor():
         g.add_edges_from(self.edges)
         largest_cc = max(nx.connected_components(g), key=len)
         convert_ids = {n: i for i, n in enumerate(largest_cc)}
-        self.edges = OrderedDict(((convert_ids[self.look_up[k[0]]], convert_ids[self.look_up[k[1]]]), v) for k, v in self.edges.items() if k[0] in largest_cc)
+        self.edges = OrderedDict(((convert_ids[k[0]], convert_ids[k[1]]), v) for k, v in self.edges.items() if k[0] in largest_cc)
         self.look_up = {k:convert_ids[v] for k, v in self.look_up.items() if v in largest_cc}
         self.demand_nodes = defaultdict(int, {convert_ids[k]:v for k, v in self.demand_nodes.items()})
         self.store_intermediate()
