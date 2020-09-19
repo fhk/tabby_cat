ed9dd2b6b3026be9aa7ecf2dee920bb6cdc7963d
plantz <f.kauker@gmail.com>
2020-09-19 13:58:45 -0700

    add else

--- tabby_cat/processor.py
+++ tabby_cat/processor.py
@@ -246,6 +246,13 @@ class Processor():
             self.edges = OrderedDict(((self.convert_ids[k[0]], self.convert_ids[k[1]]), v) for k, v in self.edges.items() if k[0] in largest_cc)
             self.look_up = {k:self.convert_ids[v] for k, v in self.look_up.items() if v in largest_cc}
             self.demand_nodes = defaultdict(int, {v:self.demand_nodes[self.flip_look_up[k]] for k, v in self.convert_ids.items()})
+        else:
+            self.g = nx.Graph()
+            self.g.add_edges_from(self.edges)
+            largest_cc = max(nx.connected_components(self.g), key=len)
+            self.flip_look_up = {v: k for k, v in self.look_up.items()}
+            self.edges = {**self.edges, **self.add_inter_demand_connections()}
+            self.edges = OrderedDict(((self.convert_ids[k[0]], self.convert_ids[k[1]]), v) for k, v in self.edges.items() if k[0] in largest_cc)
 
         demand_not_on_graph = len(self.demand) - len(self.demand_nodes)
         logging.info(f"Missing {demand_not_on_graph} points on connected graph")
