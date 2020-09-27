c0d91e39170f6986fd3178e4bfbfdccc6905f8bc
plantz <f.kauker@gmail.com>
2020-09-27 13:36:35 -0700

    move convertids

--- tabby_cat/processor.py
+++ tabby_cat/processor.py
@@ -274,10 +274,10 @@ class Processor():
         self.edges = {**self.edges, **self.add_inter_demand_connections()}
         self.g.add_edges_from(self.edges)
         largest_cc = max(nx.connected_components(self.g), key=len)
-
+        self.convert_ids = {n: i for i, n in enumerate(largest_cc)}
         self.look_up = {k:self.convert_ids[v] for k, v in self.look_up.items() if v in largest_cc}
         self.demand_nodes = defaultdict(int, {v:self.demand_nodes[self.flip_look_up[k]] for k, v in self.convert_ids.items()})
-        self.convert_ids = {n: i for i, n in enumerate(largest_cc)}
+
         self.edges = OrderedDict(((self.convert_ids[k[0]], self.convert_ids[k[1]]), v) for k, v in self.edges.items() if k[0] in largest_cc)
         
         demand_not_on_graph = len(self.demand) - len(self.demand_nodes)
