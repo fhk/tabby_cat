144c5d4cddd9f91e7f58cbf60196e1e36c006e6c
plantz <f.kauker@gmail.com>
2020-10-01 19:35:14 -0700

    recreate geom

--- tabby_cat/processor.py
+++ tabby_cat/processor.py
@@ -298,9 +298,7 @@ class Processor():
     def graph_to_geom(self, s_edges):
         edge_keys = list(self.edges)
         flip_node = {v:k for k, v in self.convert_ids.items()}
-        s_frame = pd.DataFrame([[i, self.edge_to_geom.get((flip_node[edge_keys[s][0]], flip_node[edge_keys[s][1]]), None)] for i, s in enumerate(s_edges)], columns=['id', 'geom'])
-        s_frame = s_frame.dropna()
-        s_frame['geom'] = s_frame.geom.apply(wkt.loads)
+        s_frame = pd.DataFrame([[i, LineString(self.flip_look_up[edge_keys[s][0]], self.flip_look_up[edge_keys[s][1]])] for i, s in enumerate(s_edges)], columns=['id', 'geom'])
         self.solution = gpd.GeoDataFrame(s_frame, geometry='geom', crs='epsg:3857')
 
     def load_intermediate(self):
