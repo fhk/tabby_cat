e48b03420ce01568c6ae21ee1761a17b4f16dce3
plantz <f.kauker@gmail.com>
2020-10-04 10:46:07 -0700

    double=

--- tabby_cat/processor.py
+++ tabby_cat/processor.py
@@ -238,7 +238,7 @@ class Processor():
                         id_conn[n, next_node] = cost * two_edge_cost  # Increase cost to prefer drop
                         continue
                     edge_mid = tuple(n_path[1:-1])
-                    if len(edge_mid) = 2:
+                    if len(edge_mid) == 2:
                         edge_mid_flip = edge_mid[::-1]
                         edge_length = self.edges.get(edge_mid, False)
                         if not edge_length:
