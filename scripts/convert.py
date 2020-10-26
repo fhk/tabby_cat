8a2f51e55c6c56ed0b64b2e402d516022916f625
Secret Squirrel <hello@world.com>
2020-10-26 03:06:40 +0000

    fix convert

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 import csv
 
 import geopandas as gpd
@@ -11,7 +11,10 @@ def main():
     data = data.to_crs('epsg:2163')
     data['length'] = data.geometry.apply(lambda x: x.length)
     data = data.to_crs('epsg:4326')
-    data.to_csv("network.csv")
+    part_path = os.path.basename(sys.argv[1])
+    what, _ = os.path.splitext(part_path)
+    dir_path = os.path.dirname(sys.argv[1])
+    data.to_csv(os.path.join(dir_path, what + "_network.csv"))
 
 if __name__ == "__main__":
     main()
