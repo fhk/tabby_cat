3ac76fd869fd0bcc3c55fe4749bf077c0cae2bbc
plantz <f.kauker@gmail.com>
2021-03-16 18:44:28 -0700

    maybe not a coord

--- tabby_cat/processor.py
+++ tabby_cat/processor.py
@@ -371,7 +371,7 @@ class Processor():
             self.edges = OrderedDict(((self.convert_ids[k[0]], self.convert_ids[k[1]]), v) for k, v in self.edges.items() if k[0] in largest_cc)
             self.look_up = {k:self.convert_ids[v] for k, v in self.look_up.items() if v in largest_cc}    
             self.flip_look_up = {v: k for k, v in self.look_up.items()}
-            self.demand_nodes = defaultdict(int, {self.convert_ids[k]: v for k, v in self.demand_nodes.items()})
+            self.demand_nodes = defaultdict(int, {v:self.demand_nodes[k] for k, v in self.convert_ids.items()})
 
         demand_not_on_graph = len(self.demand) - len(self.demand_nodes)
         logging.info(f"Missing {demand_not_on_graph} points on connected graph")
