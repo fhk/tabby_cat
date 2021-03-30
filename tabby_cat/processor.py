5c3fd914d618ce03ca6309e5731843dc091d5594
plantz <f.kauker@gmail.com>
2021-03-29 20:09:56 -0700

    just the ends

--- tabby_cat/processor.py
+++ tabby_cat/processor.py
@@ -356,7 +356,8 @@ class Processor():
         if not rerun:
             self.convert_ids = {n: n for n in self.g}
             self.flip_look_up = {v: k for k, v in self.look_up.items()}
-            self.nodes_to_connect = set(self.look_up[k] for k, v in self.demand_nodes.items() if v)
+            self.nodes_to_connect = set(
+                self.look_up[k] for k, v in self.demand_nodes.items() if v and self.g.degree(self.look_up[k]) == 1)
             nodes_to_connect = copy.copy(self.nodes_to_connect)
         self.add_inter_demand_connections(nearest_cost=nearest_cost)
         g_node_conn = self.add_graph_inter_demand_connections(
