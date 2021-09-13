2cbe383d508ffee19307a9d6ccd19abfc31c4cdc
fhk <4031392+fhk@users.noreply.github.com>
2021-09-13 01:03:34 -0700

    check edge cc

--- tabby_cat/processor.py
+++ tabby_cat/processor.py
@@ -381,7 +381,7 @@ class Processor():
             self.edge_to_geom.get(
                 (flip_node[edge_keys[s][0]], flip_node[edge_keys[s][1]]),
                 LineString([eval(self.flip_look_up[edge_keys[s][0]]), eval(self.flip_look_up[edge_keys[s][1]])]).wkt)]
-                for i, s in enumerate(self.edges)], columns=['id', 'start', 'end', 'geom'])
+                for i, s in enumerate(self.g.edges() if s[0] in largest_cc)], columns=['id', 'start', 'end', 'geom'])
         base_graph['geom'] = base_graph.geom.apply(wkt.loads)
         self.base_graph = gpd.GeoDataFrame(base_graph, geometry='geom', crs='epsg:3857')
 
